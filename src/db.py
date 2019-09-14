9a0c199c4fc94dcdff49415eb268324169b269b6
Jing Yao <jinghaoyao_@hotmail.com>
2019-09-14 12:29:56 -0400

    new changes

--- src/db.py
+++ src/db.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import nltk
 
 class SQliteDB:
     def __init__(self):
@@ -13,10 +14,31 @@ class SQliteDB:
         #
         self.create_table = (
             "CREATE TABLE IF NOT EXISTS TEXT_ANALYTICS_RESULTS ("
-            #
-            # TODO: Figure table schema for storing filtered entries
-            #
+            "GAME_NAME TEXT NOT NULL, "
+            "SENTIMENT INT NOT NULL, "
+            "KEYWORD TEXT NOT NULL"
             ")"
+        )
+
+        self.create_table_2 = (
+            "CREATE TABLE IF NOT EXISTS GAME_DATA_SET ("
+            "GAME_NAME TEXT NOT NULL, "
+            "GENRE TEXT NOT NULL, "
+            "REVIEWER_ID, "
+            "RATING INT NOT NULL"
+            ")"
+        )
+
+        self.create_view_command = (
+            "CREATE VIEW RESULTS AS SELECT KEYWORD, "
+            "GAME_NAME, COUNT(*) AS KEYWORD_OCCURENCES, "
+            "AVG(SENTIMENT) AS AVG_SENTIMENT "
+            "FROM TEXT_ANALYTICS_RESULTS GROUP BY KEYWORD, GAME_NAME"
+        )
+
+        self.create_view_command_2 = (
+            "CREATE VIEW FILTERED_GAME_SET AS SELECT MAX(GAME_NAME), MAX(GENRE), MAX(RATING) "
+            "FROM GAME_DATA_SET GROUP BY REVIEWER_ID"
         )
 
         #
@@ -31,6 +53,16 @@ class SQliteDB:
         #
         self.cursor.execute(self.create_table)
         self.conn.commit()
+        self.cursor.execute(self.create_table_2)
+        self.conn.commit()
+
+        #
+        # Create view
+        #
+        self.cursor.execute(self.create_view_command)
+        self.conn.commit()
+        self.cursor.execute(self.create_view_command_2)
+        self.conn.commit()
 
     def delete_all_entries(self):
         #
@@ -41,26 +73,53 @@ class SQliteDB:
         self.cursor.execute(delete_entries)
         self.conn.commit()
 
-    def insert_into_db(self, *args):
+    def insert_into_db(self, game_name, sentiment, keyword):
         #
         # Insert SQL command
         #
         insert_entries = (
             "INSERT INTO TEXT_ANALYTICS_RESULTS ("
-            #
-            # TODO: Figure table schema for storing filtered entries
-            #
+            "GAME_NAME, "
+            "SENTIMENT, "
+            "KEYWORD"
             ") "
-            "values ()"
+            "values (?, ?, ?)"
         )
 
         params = (
-            #
-            # TODO: table schema
-            #
+            game_name,
+            sentiment,
+            keyword
         )
 
         self.cursor.execute(insert_entries, params)
+        self.conn.commit()
+
+    def insert_into_db_2(self, game_name, genre, rating):
+        #
+        # Insert SQL command
+        #
+        #
+        # TODO: fix this
+        #
+        insert_entries = (
+            "INSERT INTO GAME_DATA_SET ("
+            "GAME_NAME, "
+            "GENRE, "
+            "REVIEWER_ID, "
+            "RATING"
+            ") "
+            "values (?, ?, ?)"
+        )
+
+        params = (
+            game_name,
+            genre,
+            rating
+        )
+
+        self.cursor.execute(insert_entries, params)
+        self.conn.commit()
 
     def select_from_db(self, *args):
         select_command = (
@@ -69,3 +128,7 @@ class SQliteDB:
 
         self.cursor.execute(select_command)
         self.conn.commit()
+
+    def generate_suggestions(self, game_name):
+        
+        self.cursor.execute()
--- src/textanalytics.py
+++ src/textanalytics.py
@@ -2,7 +2,11 @@ import os
 import requests
 from pprint import pprint
 from db import SQliteDB
+from pandas import DataFrame
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
 import csv
+import json
 
 class TextAnalytics:
     def init(self, _csv, _api_key, _endpoint):
@@ -17,6 +21,8 @@ class TextAnalytics:
         self.sentiment_endpoint = self.endpoint + "text/analytics/v2.1/sentiment"
         self.keyphrase_endpoint = self.endpoint + "text/analytics/v2.1/keyPhrases"
 
+        self.client = TextAnalyticsClient(endpoint=self.endpoint)
+
     def parse_csv(self):
         rows = []
         with open(self.csv_path) as csv_file:
@@ -26,13 +32,29 @@ class TextAnalytics:
 
         return rows
 
-    def send_to_text_analytics(self, rows):
-        for row in rows:
+    def send_to_text_analytics(self, data_frame):
+        for index, row in data_frame.iterrows():
+            documents = [
+                {
+                    "id": row["game_name"],
+                    "language" : "en",
+                    "text": row["review_text"]
+                }
+            ]
+
+            json_formatted = json.dumps(documents)
+
+            response_sentiment = client.sentiment(documents=documents)
+            response_key_phrases = client.key_phrase(documents=documents)
+
             #
-            # TODO: Implement azure api
+            # Extract sentiment
             #
-            
-            #
-            # TODO: Write results into db
-            #
+            extracted_sentiment_response = json.load(response_sentiment)
+            extracted_sentiment = int(extracted_sentiment_response["documents"]["score"] * 100)
+
+            for document in response_key_phrases.documents:
+                for phrase in document.key_phrases:
+                    self.db.insert_into_db(document.id, extracted_sentiment, phrase)
+
             self.db.insert_into_db()
